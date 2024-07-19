d104134804db3de31d113747823d4c39d6a43f68
Nick Woods <woodsnicholas01@gmail.com>
2024-07-19 14:01:32 +0100

    removed test file

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Trace the 'ls' command and store the output in a variable
-trace_output=$(strace ls 2>&1)
-
-# Do something with the output, for example, print it
-echo "The strace output of 'ls' is:"
